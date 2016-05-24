9a5ece044bd9f9323bebf609641b1a7a8c462b7f
LoveTube <sjs2109@gmail.com>
2016-05-24 12:58:39 +0900

    add 2 layer nn example

---
+++ etc/2_layer_nn.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+#sigmoid function
+
+def nonlin(x,deriv=False):
+	if(deriv==True):
+		return x*(1-x)
+	return 1/(1+np.exp(-x))
+
+
+x = np.array([[0,0,1],[0,1,1],[1,0,1],[1,1,1]])
+
+y = np.arrauy([[0,0,0,1]]).T
+
+np.random.seed(1)
+sync0 = 2*np.random.random((3,1)) - 1
+
+for iter in xrange(10000):
+	l0 = x
+	l1 = nonlin(np.dot(l0,sync0))
+	
+	l1_error = y -l1
+
+	l1_delta = l1_error * nonlin(l1,True)
+
+	sync0 += np.dot(l0.T,l1_delta)
+
+print("Output Afer Training")
+print(l1)
+
+
+
